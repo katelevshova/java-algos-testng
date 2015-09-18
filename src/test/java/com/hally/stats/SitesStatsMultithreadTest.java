6c90d5cceea3523020216f5385eaeecccf06d2d6
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-09-18 11:35:25 -0700

    Created SitesTatsMultithreadTest

---
+++ src/test/java/com/hally/stats/SitesStatsMultithreadTest.java
@@ -0,0 +1,55 @@
+package com.hally.stats;
+
+import com.hally.task78.TraceUtil;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Kateryna Levshova
+ *
+ * @date 17.09.2015
+ */
+public class SitesStatsMultithreadTest
+{
+	public static final String CLASS_NAME = SitesStatsMultithreadTest.class.getName();
+
+	private final String _pageNameKey1 = "a.html";
+	private SitesStats _sitesStats;
+
+	@BeforeSuite
+	public void setUp()
+	{
+		TraceUtil.logD(CLASS_NAME, "setUp", "BeforeSuite");
+		_sitesStats = new SitesStats();
+	}
+
+		// The method will be run a total of 2 times using 2 threads
+	@Test(threadPoolSize = 2, invocationCount = 2, timeOut = 10000)
+	public void testReportPageAccess2Threads()
+	{
+		Long id = Thread.currentThread().getId();
+		TraceUtil.logD(CLASS_NAME, "testReportPageAccess2Threads", "executing on thread with id: " + id);
+
+		_sitesStats.reportPageAccess(_pageNameKey1);
+	}
+
+	@AfterMethod
+	public void testSize()
+	{
+		TraceUtil.logD(CLASS_NAME, "testSize", "AfterMethod");
+		Assert.assertEquals(_sitesStats.getPages().size(), 1);
+	}
+
+	@AfterSuite (alwaysRun = false)
+	public void testValue()
+	{
+		TraceUtil.logD(CLASS_NAME, "testValue", "AfterSuite");
+		int actualValueResult = _sitesStats.getPages().get(_pageNameKey1);
+		Assert.assertEquals(actualValueResult, 2);
+	}
+}
