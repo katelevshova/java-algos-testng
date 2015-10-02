b0805b2189b290c082ab9d3fa738fc42fb81d061
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-10-01 20:59:48 -0700

    Created NumberReverser

---
+++ src/main/java/com/hally/taskSF/NumberReverser.java
@@ -0,0 +1,30 @@
+package com.hally.taskSF;
+
+/**
+ *	Task if given a number 12345 print a reverse number 54321 without String or Array.
+ *  @author Kateryna Levshova
+ * @date 01.10.2015
+ */
+public class NumberReverser
+{
+	public static final String CLASS_NAME = NumberReverser.class.getName();
+
+	public int reverse(int srcNumber)
+	{
+		int reversedNumber = 0;
+
+		while (srcNumber != 0)
+		{
+			int lastDigit = srcNumber%10;
+			reversedNumber = reversedNumber*10 + lastDigit;
+			srcNumber = srcNumber/10;
+		}
+
+		if(reversedNumber > Integer.MAX_VALUE || reversedNumber < Integer.MIN_VALUE)
+		{
+			throw new IllegalArgumentException();
+		}
+
+		return reversedNumber;
+	}
+}
---
+++ src/test/java/com/hally/taskSF/NumberReverserTest.java
@@ -0,0 +1,35 @@
+package com.hally.taskSF;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Kateryna Levshova
+ *
+ * @date 01.10.2015
+ */
+public class NumberReverserTest
+{
+	private NumberReverser _numberReverser;
+
+	@BeforeMethod(alwaysRun = true)
+	public void setUp()
+	{
+		_numberReverser = new NumberReverser();
+	}
+
+	@Test
+	public void testReverse1()
+	{
+		int actualResult = _numberReverser.reverse(12345);
+		Assert.assertEquals(actualResult, 54321);
+	}
+
+	@Test
+	public void testReverse2()
+	{
+		int actualResult = _numberReverser.reverse(-345);
+		Assert.assertEquals(actualResult, -543);
+	}
+}
