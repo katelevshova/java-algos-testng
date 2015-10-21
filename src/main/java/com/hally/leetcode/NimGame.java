9e2a2831460a51e819caf50b4b9200da620bcc98
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-10-21 10:31:05 -0700

    Init classes for NimGame task https://leetcode.com/problems/nim-game/

---
+++ src/main/java/com/hally/leetcode/NimGame.java
@@ -0,0 +1,23 @@
+package com.hally.leetcode;
+
+/**
+ * Created by Kateryna Levshova
+ *
+ * @date 02.10.2015
+ */
+public class NimGame
+{
+
+	private static final int max = 3;
+
+	public boolean canWinNim(int amount)
+	{
+		boolean result = true;
+
+		int value = 0;
+
+
+		return result;
+	}
+
+}
