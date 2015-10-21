db8095804daed0107eec0bebf511cf7851275bbb
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-10-21 14:39:10 -0700

    AddDigits - finished

---
+++ src/main/java/com/hally/leetcode/AddDigitsSolution.java
@@ -0,0 +1,26 @@
+package com.hally.leetcode;
+
+/**
+ * Created by Oni on 21.10.2015.
+ */
+public class AddDigitsSolution
+{
+	public static final String ERROR = "Num must be > 0";
+	/**
+	 *  Given a non-negative integer num, repeatedly add all its digits until the result has only
+	 *  one digit.  For example:
+	 *  Given num = 38, the process is like:
+	 *  3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+	 */
+	public int addDigits(int num) throws Exception
+	{
+		if(num > 0)
+		{
+			return num % 9;
+		}
+		else
+		{
+			throw new Exception(ERROR);
+		}
+	}
+}
---
+++ src/test/java/com/hally/leetcode/AddDigitsSolutionTest.java
@@ -0,0 +1,65 @@
+package com.hally.leetcode;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Oni on 21.10.2015.
+ */
+public class AddDigitsSolutionTest
+{
+	private AddDigitsSolution _addDigitsSolution;
+
+	@BeforeSuite
+	public void setUp()
+	{
+		_addDigitsSolution = new AddDigitsSolution();
+	}
+
+	/**
+	 * Given a non-negative integer num, repeatedly add all its digits until the result has only one
+	 * digit.  For example: Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has
+	 * only one digit, return it.
+	 */
+	@Test
+	public void testAddDigits_1()
+	{
+		try
+		{
+			Assert.assertEquals(_addDigitsSolution.addDigits(546), 6);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testAddDigits_2()
+	{
+		try
+		{
+			Assert.assertEquals(_addDigitsSolution.addDigits(382), 4);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testAddDigits_Negative()
+	{
+		try
+		{
+			_addDigitsSolution.addDigits(-56);
+			Assert.fail("Should have shown a message - " + AddDigitsSolution.ERROR);
+		}
+		catch (Exception e)
+		{
+			Assert.assertEquals(e.getMessage(), AddDigitsSolution.ERROR);
+		}
+	}
+
+}
