ee9d20bf00a25c0d9b851c2fe0a325551fd59483
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-10-28 12:44:56 -0700

    Tree Invert - best recursion solution

--- src/main/java/com/hally/leetcode/TreeInvertSolution.java
+++ src/main/java/com/hally/leetcode/TreeInvertSolution.java
@@ -10,7 +10,23 @@ import java.util.Queue;
  */
 public class TreeInvertSolution
 {
+	//Note: best implementation
 	public TreeNode invertTreeRecursion(TreeNode root)
+	{
+		if(root == null)
+		{
+			return root;
+		}
+
+		TreeNode noteTmp = root.left;
+		root.left = invertTreeRecursion(root.right);
+		root.right = invertTreeRecursion(noteTmp);
+
+		return root;
+	}
+
+	//Note: 1st implementation
+	public TreeNode invertTreeRecursionOldVersion(TreeNode root)
 	{
 		if(root == null)
 		{
@@ -51,66 +67,5 @@ public class TreeInvertSolution
 
 		TreeNode result = new TreeNode(value, leftNode, rightNode);
 		return result;
-	}
-
-	//TODO: finish this method
-	public TreeNode invertTreeIterative(TreeNode root)
-	{
-		if(root == null)
-		{
-			return null;
-		}
-
-		if(root.left == null && root.right == null)
-		{
-			return root;
-		}
-
-		Queue<TreeNode> queue = new LinkedList<>();
-		queue.offer(root.left);
-		queue.offer(root.right);
-		TreeNode result = new TreeNode(root.value);
-
-		int nodeCounter = 0;
-
-		while (true)
-		{
-			nodeCounter = queue.size();
-
-			if(nodeCounter == 0)
-			{
-				return result;
-			}
-
-			// dequeue all nodes from current level and enqueue all nodes for next level
-			while (nodeCounter > 0)
-			{
-				TreeNode leftNode = queue.poll();
-				TreeNode rightNode = queue.poll();
-
-//				TreeNode newLeft = new TreeNode(rightNode.value, rightNode.right, rightNode.left);
-//				TreeNode newRight = new TreeNode(leftNode.value, leftNode.right, leftNode.left);
-
-				TreeNode newLeft = new TreeNode(rightNode.value, rightNode.left, rightNode.right);
-				TreeNode newRight = new TreeNode(leftNode.value, leftNode.left, leftNode.right);
-
-				result.left = newLeft;
-				result.right = newRight;
-
-				if(newLeft.left != null && newLeft.right != null)
-				{
-					queue.offer(newLeft.left);
-					queue.offer(newLeft.right);
-				}
-
-				if(newRight.left != null && newRight.right != null)
-				{
-					queue.offer(newRight.left);
-					queue.offer(newRight.right);
-				}
-
-				nodeCounter -= 2;
-			}
-		}
 	}
 }
