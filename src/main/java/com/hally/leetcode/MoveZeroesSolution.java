d2f8469fd6257c25ee1827d40d38e882828a8de3
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-10-28 17:28:35 -0700

    Move Zeroes - refactoring, decreased time to 20 ms on leetcode tests

--- src/main/java/com/hally/leetcode/MoveZeroesSolution.java
+++ src/main/java/com/hally/leetcode/MoveZeroesSolution.java
@@ -8,38 +8,35 @@ import java.util.Arrays;
 public class MoveZeroesSolution
 {
 	/**
-	 * Task:
-	 *  Given an array scrArray, write a function to move all 0's to the end of it while maintaining
-	 *  the relative order of the non-zero elements.
-	 *  For example, given scrArray = [0, 1, 0, 3, 12], after calling your function,
-	 *  scrArray should be [1, 3, 12, 0, 0].
-	 *  You must do this in-place without making a copy of the array.
-	 *  Minimize the total number of operations.
+	 * Task: Given an array scrArray, write a function to move all 0's to the end of it while
+	 * maintaining the relative order of the non-zero elements. For example, given scrArray = [0, 1,
+	 * 0, 3, 12], after calling your function, scrArray should be [1, 3, 12, 0, 0]. You must do this
+	 * in-place without making a copy of the array. Minimize the total number of operations.
 	 */
 
-	//NOTE: 28 ms
+	//NOTE: 20 ms
 	public int[] moveZeroes(int[] scrArray)
 	{
-		int i = 0;
-		int length = scrArray.length;
-		int counter =0;
+		int iterator = scrArray.length;
+		int counter = 0;
 
-		while (length > 0)
+		//start search of zeroes from the tail
+		while (iterator > 0)
 		{
-			i = length - 1;
-
-			if(scrArray[i] == 0)
+			// find zero
+			if (scrArray[iterator - 1] == 0)
 			{
 				counter++;
-				int end = scrArray.length - counter;
 
-				for(int j = i; j< end; j++)
+				// rearrange all elements placed between position of zero and appropriate end
+				for (int j = iterator - 1; j < scrArray.length - counter; j++)
 				{
-					scrArray[j] = scrArray[j+1];
+					scrArray[j] = scrArray[j + 1];
 				}
-				scrArray[end] = 0;
+				// move zero to the appropriate end position
+				scrArray[scrArray.length - counter] = 0;
 			}
-			length--;
+			iterator--;
 		}
 
 		System.out.println(Arrays.toString(scrArray));
