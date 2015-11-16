f01ae3ecbcd48cbfd65562b451b9cf76aa7666da
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-11-16 15:46:05 -0800

    Hamming Weight - solutions

---
+++ src/main/java/com/hally/leetcode/HammingWeight.java
@@ -0,0 +1,52 @@
+package com.hally.leetcode;
+
+/**
+ * Created by Kateryna Levshova on 14.11.2015.
+ */
+public class HammingWeight
+{
+	/**
+	 * Task:
+	 * Write a function that takes an unsigned integer and returns the number of '1' bits it
+	 * has (also known as the Hamming weight).
+	 * For example, the 32-bit integer '11' has binary
+	 * representation 00000000000000000000000000001011,
+	 * so the function should return 3.
+	 */
+
+	public int hammingWeight(int n)
+	{
+		String binaryString = Integer.toBinaryString(n);
+
+		char[] chars = binaryString.toCharArray();
+
+		int bits1Counter = 0;
+
+		for(int i = 0; i < chars.length; i++)
+		{
+			if(chars[i] == '1')
+			{
+				bits1Counter++;
+			}
+		}
+
+		return bits1Counter;
+	}
+
+	public int hammingWeightBestSolution(int n)
+	{
+		int bits1Counter = 0;
+
+		for(int i = 0; i < 32; i++)
+		{
+			if( (n & 1) == 1)
+			{
+				bits1Counter++;
+			}
+
+			n = n >>> 1;
+		}
+
+		return bits1Counter;
+	}
+}
---
+++ src/test/java/com/hally/leetcode/HammingWeightTest.java
@@ -0,0 +1,30 @@
+package com.hally.leetcode;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Kateryna Levshova on 14.11.2015.
+ */
+public class HammingWeightTest
+{
+
+	@Test
+	public void testHammingWeight() throws Exception
+	{
+		HammingWeight hammingWeight = new HammingWeight();
+		Assert.assertEquals(hammingWeight.hammingWeight(11), 3);
+		Assert.assertEquals(hammingWeight.hammingWeight(5896), 5); // 5896 = 1011100001000
+	}
+
+	@Test
+	public void testHammingWeightBestSolution() throws Exception
+	{
+		HammingWeight hammingWeight = new HammingWeight();
+		Assert.assertEquals(hammingWeight.hammingWeightBestSolution(11), 3);
+		Assert.assertEquals(hammingWeight.hammingWeightBestSolution(5896), 5); // 5896 = 1011100001000
+//		Assert.assertEquals(hammingWeight.hammingWeightBestSolution(Integer.MAX_VALUE), 1);
+	}
+}
