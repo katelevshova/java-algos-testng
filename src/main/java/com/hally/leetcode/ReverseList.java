6eba2494ff5ff603dc178e6724a1b48b4cb4a2b4
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2015-12-06 16:44:59 -0800

    Reverse LinkedList  - https://leetcode.com/problems/reverse-linked-list/

---
+++ src/main/java/com/hally/leetcode/ReverseList.java
@@ -0,0 +1,49 @@
+package com.hally.leetcode;
+
+/**
+ * Created by Kateryna Levshova on 03.12.2015.
+ */
+public class ReverseList
+{
+	/*
+	Task: Reverse a singly linked list.
+	 */
+
+	public ListNode reverseListRecursive(ListNode head)
+	{
+		if (head == null || head.next == null)
+		{
+			return head;
+		}
+
+		ListNode root = reverseListRecursive(head.next);
+
+		head.next.next = head;
+		head.next = null;
+
+		return root;
+	}
+
+
+	public ListNode reverseListIterative(ListNode head)
+	{
+		if (head == null || head.next == null)
+		{
+			return head;
+		}
+
+		ListNode current = head;
+		ListNode tmp = null;
+		ListNode prev = null;
+
+		while (current != null)
+		{
+			tmp = current;
+			current = current.next;
+			tmp.next = prev;
+			prev = tmp;
+		}
+
+		return prev;
+	}
+}
---
+++ src/test/java/com/hally/leetcode/ReverseListTest.java
@@ -0,0 +1,54 @@
+package com.hally.leetcode;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Kateryna Levshova on 03.12.2015.
+ */
+public class ReverseListTest
+{
+	@Test
+	public void testReverseListRecursion_4_Items()
+	{
+		//1453
+		ListNode listNode4 = new ListNode(3);
+		ListNode listNode3 = new ListNode(5, listNode4);
+		ListNode listNode2 = new ListNode(4, listNode3);
+		ListNode listNode1 = new ListNode(1, listNode2);
+
+		ReverseList solution = new ReverseList();
+		String resultString = ListNode.printLinkedList(solution.reverseListRecursive(listNode1));
+
+		Assert.assertEquals(resultString, "3541");
+	}
+
+
+	@Test
+	public void testReverseListRecursion_2_Items()
+	{
+		ListNode listNode2 = new ListNode(4);
+		ListNode listNode1 = new ListNode(1, listNode2);
+
+		ReverseList solution = new ReverseList();
+		String resultString = ListNode.printLinkedList(solution.reverseListRecursive(listNode1));
+
+		Assert.assertEquals(resultString, "41");
+	}
+
+	@Test
+	public void testReverseListIterative_4_Items()
+	{
+		//1453
+		ListNode listNode4 = new ListNode(3);
+		ListNode listNode3 = new ListNode(5, listNode4);
+		ListNode listNode2 = new ListNode(4, listNode3);
+		ListNode listNode1 = new ListNode(1, listNode2);
+
+		ReverseList solution = new ReverseList();
+		String resultString = ListNode.printLinkedList(solution.reverseListIterative(listNode1));
+
+		Assert.assertEquals(resultString, "3541");
+	}
+
+}
