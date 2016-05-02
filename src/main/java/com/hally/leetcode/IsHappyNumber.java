9a49eb0fc87daa063025ef1dcde0a120788f2489
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2016-05-02 14:50:20 -0700

    https://leetcode.com/problems/happy-number/ - Is Happy number

---
+++ src/main/java/com/hally/leetcode/IsHappyNumber.java
@@ -0,0 +1,57 @@
+package com.hally.leetcode;
+
+import java.util.HashSet;
+
+/**
+ * Created by Kateryna Levshova on 02.05.2016.
+ */
+public class IsHappyNumber
+{
+	/*
+	 *
+	 Write an algorithm to determine if a number is "happy".
+
+	 A happy number is a number defined by the following process: Starting with any positive integer,
+	 replace the number by the sum of the squares of its digits, and repeat the process until the
+	 number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+	 Those numbers for which this process ends in 1 are happy numbers.
+
+	 Example: 19 is a happy number =
+
+	 1^2 + 9^2 = 82 =
+	 8^2 + 2^2 = 68 =
+	 6^2 + 8^2 = 100 =
+	 1^2 + 0^2 + 0^2 = 1
+
+	 */
+
+	public boolean isHappy(int n)
+	{
+		if(n < 1)
+		{
+			return false;
+		}
+
+		HashSet<Integer> set = new HashSet<>();
+
+		while (n != 1 && !set.contains(n))
+		{
+			set.add(n);
+			n = calculateSquareSum(n);
+		}
+
+		return (n == 1);
+	}
+
+	public int calculateSquareSum(int n)
+	{
+		int sum = 0;
+		while (n > 0)
+		{
+			int k = n % 10;
+			sum = sum + k * k;
+			n = n / 10;
+		}
+		return sum;
+	}
+}
---
+++ src/test/java/com/hally/leetcode/IsHappyNumberTest.java
@@ -0,0 +1,42 @@
+package com.hally.leetcode;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Kateryna Levshova on 02.05.2016.
+ */
+public class IsHappyNumberTest
+{
+
+	@Test
+	public void testIsHappy_True()
+	{
+		IsHappyNumber solution = new IsHappyNumber();
+		Assert.assertTrue(solution.isHappy(7));
+		Assert.assertTrue(solution.isHappy(19));
+		Assert.assertTrue(solution.isHappy(1111111));
+		Assert.assertTrue(solution.isHappy(1));
+	}
+
+	@Test
+	public void testIsHappy_False()
+	{
+		IsHappyNumber solution = new IsHappyNumber();
+		Assert.assertFalse(solution.isHappy(-7));
+		Assert.assertFalse(solution.isHappy(0));
+		Assert.assertFalse(solution.isHappy(21));
+		Assert.assertFalse(solution.isHappy(14));
+	}
+
+	@Test
+	public void testCalculateSquareSum()
+	{
+		IsHappyNumber solution = new IsHappyNumber();
+		Assert.assertEquals(solution.calculateSquareSum(97), 130);
+		Assert.assertEquals(solution.calculateSquareSum(130), 10);
+		Assert.assertEquals(solution.calculateSquareSum(81), 65);
+	}
+}
