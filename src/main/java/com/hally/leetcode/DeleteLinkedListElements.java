dccd7e923a0c6c4718db764124ab0ba1af60809c
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2016-05-02 17:22:22 -0700

    https://leetcode.com/problems/remove-linked-list-elements/ - Delete linked list elements

---
+++ src/main/java/com/hally/leetcode/DeleteLinkedListElements.java
@@ -0,0 +1,38 @@
+package com.hally.leetcode;
+
+/**
+ * Created by Kateryna Levshova on 02.05.2016.
+ */
+public class DeleteLinkedListElements
+{
+	/*
+	Remove all elements from a linked list of integers that have value val.
+
+	Example
+	Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+	Return: 1 --> 2 --> 3 --> 4 --> 5
+	*/
+
+	public ListNode deleteElements(ListNode head, int val)
+	{
+		// tmp will store 0-1-2-6-3-4-5-6 (with additional 0 in the beginning)
+		// current will be like a cursor each time
+		ListNode current, tmp = new ListNode(0);
+		current = tmp;
+		tmp.next = head;
+
+		while (current.next != null)
+		{
+			if (current.next.value == val)
+			{
+				current.next = current.next.next;  //delete the element
+			}
+			else
+			{
+				current = current.next;  // one step forward
+			}
+		}
+		return tmp.next; // since we did no move from it anywhere will return everything withou
+		// first 0
+	}
+}
---
+++ src/test/java/com/hally/leetcode/DeleteLinkedListElementsTest.java
@@ -0,0 +1,45 @@
+package com.hally.leetcode;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Kateryna Levshova on 02.05.2016.
+ */
+public class DeleteLinkedListElementsTest
+{
+
+	@Test
+	public void testDeleteElements_Middle_End()
+	{
+		DeleteLinkedListElements solution = new DeleteLinkedListElements();
+		//1-2-6-3-4-5-6
+		ListNode head = new ListNode(1, new ListNode(2, new ListNode(6, new ListNode( 3, new
+				ListNode(4, new ListNode(5, new ListNode(6)))))));
+
+		System.out.println("Input:");
+		ListNode.printLinkedList(head);
+		System.out.println("Output:");
+		String actualResult =  ListNode.printLinkedList(solution.deleteElements(head, 6));
+		Assert.assertEquals(actualResult, "12345");
+		System.out.println("-------------------");
+	}
+
+	@Test
+	public void testDeleteElements_Start_Middle_End()
+	{
+		DeleteLinkedListElements solution = new DeleteLinkedListElements();
+		//6-2-6-6-3-4-5-6
+		ListNode head = new ListNode(6, new ListNode(2, new ListNode(6, new ListNode( 6, new
+				ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))))));
+
+		System.out.println("Input:");
+		ListNode.printLinkedList(head);
+		System.out.println("Output:");
+		String actualResult =  ListNode.printLinkedList(solution.deleteElements(head, 6));
+		Assert.assertEquals(actualResult, "2345");
+		System.out.println("-------------------");
+	}
+}
