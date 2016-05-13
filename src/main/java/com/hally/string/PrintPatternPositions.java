494178ca39b0df94680b010967d3f3ca2f11b88b
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2016-05-12 17:12:59 -0700

    added a task for PrintPatternPositions

---
+++ src/main/java/com/hally/string/PrintPatternPositions.java
@@ -0,0 +1,15 @@
+package com.hally.string;
+
+/**
+ * Created by Kateryna Levshova on 12.05.2016.
+ */
+public class PrintPatternPositions
+{
+	//TODO: implement
+	/**
+	 * Task:
+	 * Given a string print all positions of word pattern in it. Function similar to indexOf()
+	 * Example: "makaroni o vse oni on voobshe"
+	 * Print: "5, 15"
+	 */
+}
