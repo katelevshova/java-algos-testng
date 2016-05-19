d2d5d3678dc6725fec09f2e8222ad1c0d4b1b742
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2016-05-18 23:35:13 -0700

    Print indexes of all occurrences of substring in a string - added 2 more solutions

--- src/main/java/com/hally/string/PrintPatternPositions.java
+++ src/main/java/com/hally/string/PrintPatternPositions.java
@@ -1,6 +1,7 @@
 package com.hally.string;
 
-import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Kateryna Levshova on 12.05.2016.
@@ -8,27 +9,27 @@ import java.util.HashMap;
 public class PrintPatternPositions
 {
 	/**
-	 * Task: Given a string print all positions of word pattern in it. Function similar to indexOf()
-	 * Example: "makaroni o vse oni on voobshe" Print: "5, 15"
+	 * Task: Given a string print all positions of word occurrences in it. Example: "makaroni o vse
+	 * oni on voobshe" Print: "5, 15"
 	 */
-	public String printPatternPositionsWithIndexOf(String source, String pattern)
+	public String printPatternPositionsWithIndexOf(String source, String findStr)
 	{
 		int lastIndex = 0;
 		String result = "";
 
-		if (pattern.length() > source.length())
+		if (findStr.length() > source.length())
 		{
 			return "";
 		}
 
 		while (lastIndex != -1)
 		{
-			lastIndex = source.indexOf(pattern, lastIndex);
+			lastIndex = source.indexOf(findStr, lastIndex);
 
 			if (lastIndex != -1)
 			{
 				result += lastIndex + ", ";
-				lastIndex += pattern.length();
+				lastIndex += findStr.length();
 			}
 		}
 
@@ -42,9 +43,85 @@ public class PrintPatternPositions
 		}
 	}
 
-	public String printPatternPositions(String source, String pattern)
+	public String printPatternPositionsRegEx(String source, String findStr)
 	{
 		String result = "";
-		return result;
+
+		if (findStr.length() > source.length())
+		{
+			return "";
+		}
+
+		Pattern pattern = Pattern.compile(findStr);
+		Matcher matcher = pattern.matcher(source);
+
+		while (matcher.find())
+		{
+			result += matcher.start() + ", ";
+		}
+
+		if (result == "")
+		{
+			return result;
+		}
+		else
+		{
+			return result.substring(0, result.length() - 2);
+		}
+	}
+
+	// the worst solution. It also has 2 failed tests
+	public String printPatternPositions(String source, String findStr)
+	{
+		String result = "";
+
+		if (findStr.length() > source.length())
+		{
+			return "";
+		}
+
+		boolean flag = false;
+		int index = 0;
+
+		for (int i = 0; i < source.length(); i++)
+		{
+			for (int j = 0; j < findStr.length(); j++)
+			{
+				if(  source.charAt(i) == findStr.charAt(j))
+				{
+					if(flag == false)
+					{
+						index = i;
+					}
+
+					if(i != source.length() -1)
+					{
+						i++;
+					}
+
+					flag = true;
+				}
+				else
+				{
+					flag = false;
+					break;
+				}
+
+				if(flag == true && j == findStr.length()-1)
+				{
+					result += index + ", ";
+					flag = false;
+				}
+			}
+		}
+
+		if (result == "")
+		{
+			return result;
+		}
+		else
+		{
+			return result.substring(0, result.length() - 2);
+		}
 	}
 }
--- src/test/java/com/hally/string/PrintPatternPositionsTest.java
+++ src/test/java/com/hally/string/PrintPatternPositionsTest.java
@@ -3,8 +3,6 @@ package com.hally.string;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 /**
  * Created by Kateryna Levshova on 18.05.2016.
  */
@@ -21,5 +19,33 @@ public class PrintPatternPositionsTest
 		Assert.assertEquals(solution.printPatternPositionsWithIndexOf("vo vo", "vo vo"), "0");
 		Assert.assertEquals(solution.printPatternPositionsWithIndexOf("vvoo mjk", "vo"), "1");
 		Assert.assertEquals(solution.printPatternPositionsWithIndexOf("vvoo mjkva", "vo"), "1");
+		Assert.assertEquals(solution.printPatternPositionsWithIndexOf("23232", "232"), "0");
+	}
+
+
+	@Test
+	public void testPrintPatternPositionsRegEx() throws Exception
+	{
+		PrintPatternPositions solution = new PrintPatternPositions();
+		Assert.assertEquals(solution.printPatternPositionsRegEx("vo vo", "vo"), "0, 3");
+		Assert.assertEquals(solution.printPatternPositionsRegEx("vo vo", "voto"), "");
+		Assert.assertEquals(solution.printPatternPositionsRegEx("vovo", "vovovovo"), "");
+		Assert.assertEquals(solution.printPatternPositionsRegEx("vo vo", "vo vo"), "0");
+		Assert.assertEquals(solution.printPatternPositionsRegEx("vvoo mjk", "vo"), "1");
+		Assert.assertEquals(solution.printPatternPositionsRegEx("vvoo mjkva", "vo"), "1");
+		Assert.assertEquals(solution.printPatternPositionsRegEx("23232", "232"), "0");
+	}
+
+	@Test
+	public void testPrintPatternPositions() throws Exception
+	{
+		PrintPatternPositions solution = new PrintPatternPositions();
+		Assert.assertEquals(solution.printPatternPositions("azvo vo", "vo"), "2, 5");
+		Assert.assertEquals(solution.printPatternPositions("vo vo", "voto"), "");
+		Assert.assertEquals(solution.printPatternPositions("vovo", "vovovovo"), "");
+		Assert.assertEquals(solution.printPatternPositions("vo vo", "vo vo"), "0");
+		Assert.assertEquals(solution.printPatternPositions("vvoo mjk", "vo"), "1"); 	// fail
+		Assert.assertEquals(solution.printPatternPositions("vvoo mjkva", "vo"), "1");	//fail
+		Assert.assertEquals(solution.printPatternPositions("23232", "232"), "0");
 	}
 }
